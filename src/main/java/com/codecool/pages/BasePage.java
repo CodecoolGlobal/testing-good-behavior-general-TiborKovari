67e3271731e4a35f89541f89c25fba5c471a5987
andrastoth021 <andrastoth021@gmail.com>
2024-08-06 13:24:10 +0200

    add and implement page object for loginPage

--- src/main/java/com/codecool/pages/BasePage.java
+++ src/main/java/com/codecool/pages/BasePage.java
@@ -1,11 +1,16 @@
 package com.codecool.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public abstract class BasePage {
-    WebDriver webDriver;
+    protected WebDriver webDriver;
+    protected WebDriverWait wait;
 
     public BasePage(WebDriver webDriver) {
         this.webDriver = webDriver;
+        wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
     }
 }
