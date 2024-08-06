73f91fdcd3f3daf4759b06ec7b531a22a764dfc3
andrastoth021 <andrastoth021@gmail.com>
2024-08-06 10:58:47 +0200

    refactor the structure for Page objects and tests

--- src/main/java/com/codecool/pages/BasePage.java
+++ src/main/java/com/codecool/pages/BasePage.java
@@ -1,4 +1,11 @@
 package com.codecool.pages;
 
+import org.openqa.selenium.WebDriver;
+
 public abstract class BasePage {
+    WebDriver webDriver;
+
+    public BasePage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
 }
