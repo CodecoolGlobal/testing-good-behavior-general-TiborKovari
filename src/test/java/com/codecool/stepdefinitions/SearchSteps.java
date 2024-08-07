8767b8d90f0ad46e05a79a703d49f9ead7dc2c4e
TiborKovari <142129834+TiborKovari@users.noreply.github.com>
2024-08-07 11:30:10 +0200

    pom xml corrected with cucumber and seachsteps created

--- src/test/java/com/codecool/stepdefinitions/SearchSteps.java
+++ src/test/java/com/codecool/stepdefinitions/SearchSteps.java
@@ -1,4 +1,20 @@
 package com.codecool.stepdefinitions;
 
+import com.codecool.pages.AllGamesPage;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import static org.junit.Assert.assertTrue;
+
+
 public class SearchSteps {
 }
