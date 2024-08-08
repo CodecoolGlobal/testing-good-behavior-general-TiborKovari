a4bc0dd3f04e5599966da7fed4178990309ebfcd
TiborKovari <142129834+TiborKovari@users.noreply.github.com>
2024-08-08 10:51:15 +0200

    code refactored and working

--- src/test/java/com/codecool/stepdefinitions/SearchSteps.java
+++ src/test/java/com/codecool/stepdefinitions/SearchSteps.java
@@ -9,6 +9,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -24,15 +25,16 @@ public class SearchSteps extends Utils {
     private AllGamesPage allGamesPage;
     private HomeLoggedInPage homeLoggedInPage;
     private HomePage homePage;
-    private final String username = "mmaakkkmmarcci";
-    private final String email = "mmaakkkmmarcci@jojo.com";
+    private final String username = "mmmaaakkkmmarcci";
+    private final String email = "mmmaaakkkmmarcci@jojo.com";
     private final String password = "Jojo8888";
     private final String gameName = "Twilight Imperium: Fourth Edition";
 
     @Given("the user is on the all games page")
     public void the_user_is_on_the_all_games_page() {
         openNewDriver("register");
-        String alertMessage = registerUser(username, email, password);
+        registerUser(username, email, password);
+        String alertMessage = getAlertMessage();
         if (!validateRegistrationSuccess(alertMessage)) {
             throw new RuntimeException("Registration failed");
         }
@@ -45,14 +47,14 @@ public class SearchSteps extends Utils {
         }
 
         homeLoggedInPage = new HomeLoggedInPage(webDriver);
+        By welcomeMessageLocator = By.xpath("//b[text()='" + username + "']");
+        waitForElementVisibility(welcomeMessageLocator);
         homeLoggedInPage.clickAllGamesButton();
-        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
         allGamesPage = new AllGamesPage(webDriver);
     }
 
     @When("the user clicks on the advanced search button")
     public void the_user_clicks_on_the_advanced_search_button() {
-        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
         allGamesPage.clickOnAdvancedSearch();
     }
 
@@ -63,7 +65,6 @@ public class SearchSteps extends Utils {
 
     @Then("the user should see valid game name in the search results")
     public void the_user_should_see_valid_game_name_in_the_search_results() {
-        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(2));
         assertEquals(gameName, allGamesPage.getFirstResultLinkText());
         quitDriver();
     }
